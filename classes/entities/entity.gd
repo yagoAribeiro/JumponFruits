f2354aeb19573aaf605e5960a08f711d0783924e
YagoA <yagoamancio10@gmail.com>
2023-09-13 22:04:12 -0300

    (new) #1 Implementado movimentação completa do jogador

--- classes/entities/entity.gd
+++ classes/entities/entity.gd
@@ -4,19 +4,24 @@ class_name Entity
 @export var life_points: int
 @export var speed: float
 @export var weight: float = 1.0
-
+@export var animation: AnimationPlayer
+@export var collision: CollisionShape2D
 const State = {
 	Dead = 0,
 	Moving = 1,
 	Idle = 2
 }
 
-@onready var animation: AnimationPlayer = get_node("animation")
-@onready var collision: CollisionShape2D = get_node("collision")
+const LookDirection = {
+	Left = -1,
+	Right = 1
+}
+
 @onready var default_gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity", 0) as float
 
 var can_die: bool = false
 var current_state: int = State.Idle
+var current_direction: int = LookDirection.Left
 
 func _process(delta):
 	disposer()
@@ -34,7 +39,7 @@ func apply_gravity(delta: float) -> void:
 	velocity.y += delta*default_gravity*weight
 
 func move_behavior(_delta: float) -> void:
-	pass
+	current_direction = LookDirection.Left if velocity.x < 0 else LookDirection.Right if velocity.x > 0 else current_direction
 
 func action_behavior(_delta: float) -> void:
 	pass
--- classes/entities/player/player.gd
+++ classes/entities/player/player.gd
@@ -2,22 +2,42 @@ extends Entity
 class_name Player
 
 @export var jump_force: float
+@export var wall_direction: RayCast2D
+@export var wall_timer: Timer
+@export var total_wall_time: float = 5.0
 var jumps: int = 2
 
-func _ready():
-	pass
-
-func move_behavior(_delta:float) -> void:
+func move_behavior(delta:float) -> void:
+	super.move_behavior(delta)
+	wall_direction.target_position.x = abs(wall_direction.target_position.x)*current_direction
 	var direction:Vector2 = Vector2.ZERO
 	direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	velocity.x = lerp(velocity.x, direction.x*speed, 0.25)
 
 func action_behavior(delta:float) -> void:
 	jumps = 2 if is_on_floor() else 1 if jumps > 0 else 0
-	jump(delta)
+	jump(delta, wall_sliding(delta))
 
-func jump(_delta: float) -> void:
+func jump(_delta: float, on_wall: bool = false) -> void:
 	if jumps > 0 and Input.is_action_just_pressed("jump"):
 		velocity.y = 0
 		velocity.y = lerp(up_direction.y*jump_force, 0.0, 0.2)
 		jumps -=1
+		if on_wall:
+			velocity.x = 430*-current_direction
+
+
+func wall_sliding(delta:float) -> bool:
+	wall_direction.force_raycast_update()
+	if is_on_floor_only(): wall_timer.start(total_wall_time)
+	wall_timer.paused = true
+	var can_grab: bool =  is_on_wall_only() and velocity.x !=0 and velocity.y !=0 and velocity.y > up_direction.y
+	if  can_grab and wall_direction.is_colliding():
+		wall_timer.paused = false
+		if wall_timer.time_left > 0:
+			jumps = 1
+			velocity.y = abs(default_gravity-wall_timer.time_left*2)*3*delta
+			return true
+	return false
+
+	
